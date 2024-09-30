af85ba48b95b2ebb8ed7fbf62076313f27a52a31
belka.boo <ibelka.boo@gmail.com>
2024-09-30 17:31:21 +0300

    1_sem_hw add solution

--- 1_SEM_HW/index.js
+++ 1_SEM_HW/index.js
@@ -6,5 +6,63 @@
 — * На каждой странице реализуйте счетчик просмотров. Значение счетчика должно увеличиваться на единицу каждый раз, когда загружается страница.
 */
 
+const http = require('http');
+const httpPort = 3000;
+let homePageViewsCounter = 0;
+let aboutPageViewsCounter = 0;
 
+const httpServer = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+
+    if (req.url === '/') {
+
+        homePageViewsCounter++;
+
+        res.writeHead(200);
+        res.end(`
+            <html>
+                <head><title>Home Page</title></head>
+                <body>
+                    <h1>Home Page</h1>
+                    <p>Кол-во просмотров: ${homePageViewsCounter}.</p>
+                    <a href="/about">About</a>
+
+                </body>
+            </html>
+        `);
+    }
+    else if (req.url === '/about') {
+
+        aboutPageViewsCounter++;
+
+        res.writeHead(200);
+        res.end(`
+            <html>
+                <head><title>About</title></head>
+                <body>
+                    <h1>About</h1>
+                    <p>Кол-во проосмотров: ${aboutPageViewsCounter}.</p>
+                    <a href="/">Home page</a>
+                </body>
+            </html>
+        `);
+    }
+    else {
+        res.writeHead(404);
+        res.end(`
+            <html>
+                <head><title>Страница не найдена</title></head>
+                <body>
+                    <h1>Ooops! Страница не найдена...</h1>
+                    <a href="javascript:history.back()">Вернуться назад</a>
+                </body>
+            </html>
+            `)
+    }
+});
+
+
+httpServer.listen(httpPort, () => {
+    console.log(`Сервер запущен на http://127.0.0.1:${httpPort}`);
+});
 
